dda4827ff9a4c4b2b236e5c637b9e75b3eab70a3
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2023-11-22 10:12:38 -0300

    feat: organização do projeto

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet" />
+    <link rel="stylesheet" href="body/body.css">
+    <link rel="stylesheet" href="slideBar/slideBar.css">
     <link rel="stylesheet" href="../assets/css/dashboard.css">
     <link rel="icon" type="image/x-icon" href="../assets/img/faviconImg.ico">
     <title>Lú Salgados</title>
