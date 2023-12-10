94886ef5cfb31f655544bf7bd0f29d3a404fefee
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2023-12-10 18:05:14 -0300

    fix: errors and issues

--- index.php
+++ index.php
@@ -1,46 +1,7 @@
 <?php
-//login  Criação da Branch 19/11/2023 as 01:53hrs
+include_once './config/conexao.php';
 
-include('config/conexao.php');
-
-if (isset($_POST['email']) && isset($_POST['senha'])) {
-    if (strlen($_POST['email']) == 0) {
-        "";
-    } else if (strlen($_POST['senha']) == 0) {
-        "";
-    } else {
-        $email = $conn->real_escape_string($_POST['email']);
-        $senha = $conn->real_escape_string($_POST['senha']);
-
-        $sql_code = "SELECT * FROM users WHERE user_email = '$email' AND user_password = '$senha'";
-        $sql_query = $conn->query($sql_code) or die("Falha na execução do Código SQL:" . $conn->error);
-
-        $quantidade = $sql_query->num_rows;
-
-        if ($quantidade == 1) {
-            $usuario = $sql_query->fetch_assoc();
-
-            if (!isset($_SESSION)) {
-                session_start();
-            }
-
-            $_SESSION["id"] = $usuario['id'];
-            $_SESSION["nome"] = $usuario['nome'];
-
-            if (isset($_POST['manter_conectado'])) {
-                // Defina um cookie para manter a sessão ativa por um período mais longo
-                setcookie("usuario_id", $usuario['id'], time() + (30 * 24 * 60 * 60)); // Expira em 30 dias
-            }
-
-            header('Location: pages/dashboard.php');
-        } else {
-            echo "";
-        }
-    }
-}
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -50,7 +11,8 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1tzjvRp9Uq/8yFAalTTG0zXxPqe5i" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
@@ -64,60 +26,42 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
             <h1> Faça seu login </h1>
 
             <?php
-            $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT );
-           
-            if(!empty($dados['AddMsgCont'])) { 
-             //var_dump($dados);
+            $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-            //aqui ocorrerá a validação dos dados individualmente
-            if (empty($dados['email']))
-            {
-                echo"<p style= 'color: #f00;'>Erro: É necessário preencher o campo e-mail.</p> ";
-            } elseif(empty($dados['senha'])){
-                echo"<p style= 'color: #f00;'>Erro: É necessário preencher o campo de senha.</p> ";
-            }
-            else {
+            if (!empty($dados['AddMsgCont'])) {
+                //var_dump($dados);
             
-            $query_contato = "INSERT INTO contatos (email, senha) VALUES (:email, :senha)";
-            $add_contato = $conn-> prepare($query_contato);
-            $add_contato->bindParam(':email', $dados['email'], PDO:: PARAM_STR);
-            $add_contato->bindParam(':senha', $dados['senha'], PDO:: PARAM_STR);
-            $add_contato->execute();
+                //aqui ocorrerá a validação dos dados individualmente
+                if (empty($dados['email'])) {
+                    echo "<p style= 'color: #f00;'>Erro: É necessário preencher o campo e-mail.</p> ";
+                } elseif (empty($dados['senha'])) {
+                    echo "<p style= 'color: #f00;'>Erro: É necessário preencher o campo de senha.</p> ";
+                } else {
 
-            if($add_contato-> rowCount()) {
-            unset($dados);
-            echo "<p style = 'color: green;'> Em breve será redirecionado para a página inicial.</p>"; 
-            } else {
-            echo "Não foi possível acessar a página inicial."; 
-        }
-        }
-    }
-            ?>
+                    $query_contato = "INSERT INTO users (user_email, user_password) VALUES (:email, :senha)";
+                    $add_contato = $conn->prepare($query_contato);
+                    $add_contato->bindParam(':email', $dados['email'], PDO::PARAM_STR);
+                    $add_contato->bindParam(':senha', $dados['senha'], PDO::PARAM_STR);
+                    $add_contato->execute();
 
-            <?php 
-            $email = "";
-            if(isset($dados['email']))
-            { 
-                $email =  $dados['email'];
+                    if ($add_contato->rowCount()) {
+                        unset($dados);
+                        echo "<p style = 'color: green;'> Em breve será redirecionado para a página inicial.</p>";
+                    } else {
+                        echo "Não foi possível acessar a página inicial.";
+                    }
+                }
             }
             ?>
 
-            <?php 
-            $senha = "";
-            if(isset($dados['senha']))
-            { 
-                $senha =  $dados['senha'];
-            }
-            ?>
-
-
+      
             <div class="input-box">
-                <input type="text" name="email" placeholder="Email" value="<?php  echo $email; ?>" id="Login"  >
+                <input type="text" name="email" placeholder="Email" value="<?php echo $email; ?>" id="Login">
                 <i class='bx bxs-user' style='color:#ff7500'></i>
             </div>
 
             <div class="input-box">
-                <input type="password" name="senha" placeholder="Senha" value="<?php echo $senha; ?>" id="Senha" >
+                <input type="password" name="senha" placeholder="Senha" value="<?php echo $senha; ?>" id="Senha">
                 <i class='bx bxs-lock-alt' style='color:#ff7500'></i>
             </div>
 
@@ -125,7 +69,7 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
                 <label><input type="checkbox">Lembrar-se de mim </label>
             </div>
 
-            <input type="submit" name= "AddMsgCont" clas= btn value= "Enviar" >  
+            <input type="submit" name="AddMsgCont" clas=btn value="Enviar">
             <div class="register-link">
             </div>
 
