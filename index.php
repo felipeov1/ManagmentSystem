c12906ffaeada1cac5b1b7c1efc83f12f6f3f352
RuiNeto0303 <ruimasterepic@gmail.com>
2023-12-20 19:38:51 -0300

    teste commit

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include_once './config/conexao.php';
 
 <body>
 
-
+<p>rui esteve aqui</p>
     <div class="wrapper">
         <form action="" method="post">
             <h1> Faça seu login </h1>
@@ -91,4 +91,5 @@ include_once './config/conexao.php';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
 </html>
