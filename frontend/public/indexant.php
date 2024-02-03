e8e58c6b8d0f0c1bb7d9bb74a5b91524afe971f6
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2024-02-03 11:42:06 -0300

    fix: css error

--- frontend/public/indexant.php
+++ frontend/public/indexant.php
@@ -1,9 +1,9 @@
 <?php
-// include_once './config/conexao.php';
+include_once './config/conexao.php';
 
-require '../vendor/autoload.php';
-require 'bootstrap.php';
-router();
+// require '../vendor/autoload.php';
+// require 'bootstrap.php';
+// router();
 ?>
 
 
@@ -17,7 +17,9 @@ router();
     <title>Tela de Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1tzjvRp9Uq/8yFAalTTG0zXxPqe5i" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <!-- <link rel="stylesheet" href="assets/css/login.css"> -->
+    <link rel="stylesheet" href="styles.css">
+
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
